64d874424630f0370b5998040ccf2d69aa12fba5
Mark Glasgow <markglasgow@gmail.com>
2019-02-25 10:49:28 +0000

    task3 - mkdir + cd

--- Lab04_Task3.sh
+++ Lab04_Task3.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-mkdir -p ${1} && cd ${$1};
+# Mark Glasgow : 2336003G
+
+# Write a function (Task3.sh) which takes a directory name as a parameter and:
+# a) Checks if the directory already exists 
+# b) Create it if it does not exist
+# c) Moves into the directory
+
+# Must be run with the dot command (.) as .sh only effects the subshell
+# Alternatively, an alias can be used. 
+
+if [ ! -d ${1} ]
+    then
+    mkdir -p ${1} && cd ${1};
+    else
+	echo "Folder already exists"
+fi
+
+
+    
 
